d43bf5ac390ede933c94eefb1a783ecd70b82589
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-03-27 12:12:42 +0100

    Firefox only

--- windows/README.md
+++ windows/README.md
@@ -5,3 +5,8 @@ Run with:
 ```powershell
 iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/danielschwensen/system-init/master/windows/setup.ps1'))
 ```
+
+Firefox only
+```powershell
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/danielschwensen/system-init/master/windows/setup_FF_only.ps1'))
+```
---
+++ windows/setup_FF_only.ps1
@@ -0,0 +1,43 @@
+function Install-Chocolatey {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function Install-FromChocolatey {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $PackageName
+    )
+
+    choco install $PackageName --yes
+}
+
+function Install-PowerShellModule {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $ModuleName,
+
+        [ScriptBlock]
+        [Parameter(Mandatory = $true)]
+        $PostInstall = {}
+    )
+
+    if (!(Get-Command -Name $ModuleName -ErrorAction SilentlyContinue)) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force;
+        Write-Host "Installing $ModuleName"
+        Install-Module -Name $ModuleName
+        Import-Module $ModuleName
+
+        Invoke-Command -ScriptBlock $PostInstall
+    } else {
+        Write-Host "$ModuleName was already installed, skipping"
+    }
+}
+
+Install-Chocolatey
+#Install-FromChocolatey 'vscode'
+#Install-FromChocolatey 'microsoft-windows-terminal'
+Install-FromChocolatey 'firefox'
